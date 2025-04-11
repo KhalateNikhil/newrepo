c61132183436fd7770c39e6eb3663a34c14ba119
KhalateNikhil <nsknikhilkhalate@gmail.com>
2025-04-11 15:03:09 +0530

    PIM Page TC added by Tester2

---
+++ src/test/java/TestLayer/PIMPageTest.java
@@ -0,0 +1,15 @@
+package TestLayer;
+
+import org.testng.annotations.Test;
+
+import BaseLayer.BaseClass;
+
+public class PIMPageTest extends BaseClass{
+
+	@Test
+	public void PimPageTC() 
+	{
+		System.out.println("PIM page TC added");
+	}
+	
+}
