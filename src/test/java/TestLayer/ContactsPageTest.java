d55f9862a003aa7433b2421abc5f9c918512f13f
KhalateNikhil <nsknikhilkhalate@gmail.com>
2025-04-11 15:11:01 +0530

    Contacts Page TC created by Tester1

---
+++ src/test/java/TestLayer/ContactsPageTest.java
@@ -0,0 +1,14 @@
+package TestLayer;
+
+import org.testng.annotations.Test;
+
+import BaseLayer.BaseClass;
+
+public class ContactsPageTest extends BaseClass{
+
+	@Test
+	public void ContactsPageTC()
+	{
+		System.out.println("Contacts page TC added");
+	}
+}
