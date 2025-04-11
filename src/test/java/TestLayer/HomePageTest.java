2ddf69ba65d093b4f522fccd2f85b7b61b0366a0
KhalateNikhil <nsknikhilkhalate@gmail.com>
2025-04-11 14:58:07 +0530

    HomePage TC added by Tester1

---
+++ src/test/java/TestLayer/HomePageTest.java
@@ -0,0 +1,15 @@
+package TestLayer;
+
+import org.testng.annotations.Test;
+
+import BaseLayer.BaseClass;
+
+public class HomePageTest extends BaseClass{
+
+	@Test
+	public void HomePageTC()
+	{
+		System.out.println("Home page test case added");
+	}
+	
+}
